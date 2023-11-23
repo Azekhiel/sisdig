2630b68e4ea924aea5f00ca2b9bcad95b57d2597
azfaray <105913758+azfaray@users.noreply.github.com>
2023-11-23 14:42:13 +0700

    Add files via upload

--- dectobin.vhd
+++ dectobin.vhd
@@ -1,49 +1,86 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
-use ieee.std_logic_arith.all;
 use ieee.numeric_std.all;
 
 entity dectobin is
     port ( 
-        int: in integer;
-        float_in: in std_logic_vector(31 downto 0); -- Input floating-point value
-        out_bin: out std_logic_vector(15 downto 0)
+        int_in: in integer;
+        float_in: in integer; 
+        out_bin: out std_logic_vector(14 downto 0)
     );
 end dectobin;
 
-architecture behavioral of dectobcd is
-    signal temp: integer;
+architecture behavioral of dectobin is
+    signal int: integer;
     signal float1: std_logic_vector(10 downto 0);
     signal int1: std_logic_vector(10 downto 0);
-    signal float: std_logic_vector(37 downto 0); -- Adjusted to 37 bits
+    signal float: integer;
+    signal out_signal: std_logic_vector(21 downto 0);
+    signal l: integer;
+    signal k: std_logic;
+	 signal exp : std_logic_vector (4 downto 0);
+	 signal mantissa : std_logic_vector (9 downto 0);
 
 begin
-    float <= '0' & float_in & "0000"; -- Padding '0' at MSB and "0000" at LSB to make it 37-bit
 
-    process (int, float)
+    process (int, float_in)
+        variable int: integer;
+        variable float1: std_logic_vector(10 downto 0);
+        variable int1: std_logic_vector(10 downto 0);
+        variable float: integer;
+        variable l: integer;
+        variable k: std_logic;
+		  variable exp : std_logic_vector (4 downto 0);
+		  variable mantissa : std_logic_vector (9 downto 0);
+
     begin
+        int := int_in;
+        float := float_in;
+        
         -- Integer part to BCD
-        if int mod 2 = 1 then
-            int1(0) <= '1';
-        else
-            int1(0) <= '0';
-        end if;
-
-        -- Mantissa part to BCD
         for i in 0 to 10 loop
-            if float(36) = '1' then -- Check the sign bit for float
-                float <= float(35 downto 0) + "1"; -- Add 1 to the mantissa if negative
-            end if;
-
-            if float(35) = '1' then
-                float <= float(34 downto 0) & '0'; -- Shift mantissa left if MSB is 1
+            if int mod 2 = 1 then
+                int1(i) := '1';
             else
-                float <= '0' & float(34 downto 0); -- Shift mantissa left if MSB is 0
+                int1(i) := '0';
             end if;
-
-            float1(i) <= float(36); -- Store MSB of mantissa in BCD
+            int := int / 2;
         end loop;
 
-    end process;
+        -- Float part to BCD
+        if float > 0 then
+            while float < 1000000000 and float > 0 loop
+                float := float * 10;
+            end loop;
+            
+            float := float * 2;
+            for j in 10 downto 0 loop
+                if float / 10000000000 = 1 then
+                    float1(j) := '1';
+                else
+                    float1(j) := '0';
+                end if;
+                
+                if float < 10000000000 then
+                    float := float - 10000000000;
+                end if;
+            end loop;
+        end if;
+        
+        out_signal <= int1 & float1;
+        
+        k := out_signal(21);
+        l := 21;
+        while k = '0' and l > 0 loop
+            l := l - 1;
+            k := out_signal(l);
+        end loop;
+			
+		exp := "00000" + (l - 1) + 15 - 11;
+		mantissa := out_signal((l - 1) downto (l - 10));
+		
+    
+	 end process;
+
 end behavioral;
