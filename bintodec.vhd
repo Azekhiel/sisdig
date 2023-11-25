0347f0ca3e96cfed9965d1a4b1f84328a80ffd76
azfaray <105913758+azfaray@users.noreply.github.com>
2023-11-25 19:33:31 +0700

    Update bintodec.vhd

--- bintodec.vhd
+++ bintodec.vhd
@@ -12,10 +12,12 @@ end bintodec;
 
 architecture behavioral of bintodec is
 
-signal init : integer;
-signal init2 : integer;
+signal init : integer := 0;
+signal init2 : integer := 0;
 signal hasil_dec : integer;
 signal hasil_mant : integer;
+signal kali1 : integer := 0;
+signal kali2 : integer := -1;
 
 begin
 
@@ -23,27 +25,41 @@ process(inp_bin)
 variable int1 : integer;
 variable int2 : integer;
 
+
 begin
-	for i in 27 downto 22 loop
-		for j in 5 downto 0 loop
-			if inp_bin(i) = '1' then
-				int1 := (2**j);
-				hasil_dec <= init + int1;
-				init <= int1;
-			end if;
-		end loop;
+	for i in 22 to 27 loop
+		if inp_bin(i) = '1' then
+			int1 := (2**kali1);
+			kali1 <= kali1 + 1;
+			hasil_dec <= init + int1;
+			init <= int1;
+		end if;
 	end loop;
 
-	for k in 21 downto 0 loop
-		for u in -1 downto -22 loop
-			if inp_bin(k) = '1' then
-				int2 := (2**u);
-				hasil_mant <= init2 + int2;
-				init2 <= int2;
-			end if;
-		end loop;
+	for j in 21 downto 0 loop
+		if inp_bin(j) = '1' then
+			int2 := (2**kali2);
+			kali2 <= kali2 - 1;
+			hasil_mant <= init2 + int2;
+			init2 <= int2;
+		end if;
 	end loop;
 	
 end process;
 end behavioral;
-	
+
+		-- for j in 5 downto 0 loop
+			-- if inp_bin(i) = '1' then
+				-- int1 := (2**j);
+				-- hasil_dec <= init + int1;
+				-- init <= int1;
+			-- end if;
+		-- end loop;
+		
+		-- for u in -1 downto -22 loop
+			-- if inp_bin(k) = '1' then
+				-- int2 := (2**u);
+				-- hasil_mant <= init2 + int2;
+				-- init2 <= int2;
+			-- end if;
+		-- end loop;
