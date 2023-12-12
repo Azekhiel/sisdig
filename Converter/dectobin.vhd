dc1e988a80fbe10e24997f43917573edb7840547
William <115348219+Azekhiel@users.noreply.github.com>
2023-12-13 05:21:15 +0700

    Update dectobin.vhd

--- Converter/dectobin.vhd
+++ Converter/dectobin.vhd
@@ -25,6 +25,8 @@ architecture behavioral of dectobin is
     signal exp : integer;
     signal mantissa : std_logic_vector (9 downto 0);
     signal exp_sementara: std_logic_vector (4 downto 0);
+	 		  signal zero : std_logic_vector (9 downto 0) := "0000000000";
+
 
 
 begin
@@ -35,7 +37,7 @@ begin
         variable int1: std_logic_vector(10 downto 0);
         variable float: integer;
         variable l: integer;
-        variable k: std_logic;
+       -- variable k: std_logic;
         variable exp : integer;
         variable mantissa : std_logic_vector (9 downto 0);
         variable exp_sementara: std_logic_vector (4 downto 0);
@@ -51,42 +53,49 @@ begin
         end loop;
 
         if float > 0 then
-            while float < 1000000 and float > 0 loop
-                float := float * 10;
-            end loop;
-            
+
+				
+            for j in 10 downto 0 loop            
             float := float * 2;
-            for j in 10 downto 0 loop
-                if float / 1000000 = 1 then
+                if float / 100000 = 1 then
                     float1(j) := '1';
                 else
                     float1(j) := '0';
                 end if;
                 
-                if float < 1000000 then
-                    float := float - 1000000;
+                if float > 100000 then
+                    float := float - 100000;
                 end if;
             end loop;
-        end if;
         
         out_signal <= int1 & float1;
         
-        k := out_signal(21);
+        k <= out_signal(21);
         l := 21;
-        for l in 21 downto 0 loop
+        for m in 21 downto 0 loop
 			if (k = '0') then
-				k := out_signal(l);
+				k <= out_signal(m);
 			else 
+				l := m;
 				exit;
 			end if;
         end loop;
         
-        exp_sementara := "00000";
+        --exp_sementara := "00000";
         
-        exp := to_integer(unsigned(exp_sementara(4 downto 0))) + (l - 1) + 15 - 11;
-        mantissa := out_signal((l - 1) downto (l - 10));
+        exp := (l - 11) + 15;
+		  if l>=10 then
+        mantissa := out_signal((l -1) downto (l - 10));
+		  else
+		  zero (9 downto (10-l)) <= out_signal((l-1) downto 0);
+			mantissa := zero;
+		  end if;
 		  
 		out_bin <= sign_in & std_logic_vector(to_unsigned(exp, 5)) & mantissa;
+		
+		else
+		out_bin<= "0000000000000000";
+		end if;
 
     end process;
 
